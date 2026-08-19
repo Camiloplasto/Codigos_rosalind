206214b5ff1b8a36de22d9e31682dfe0c90b80b9
Camilo Andres Marin <cmarincastaneda@gmail.com>
2026-08-19 14:00:26 +0200

    Solución del es problema Rosalind SPLC (RNA Splicing) utilizando Biopython. El código lee un archivo de entrada en formato FASTA, extrae la secuencia principal y los intrones, elimina los intrones de la secuencia principal y luego traduce la secuencia resultante a una cadena de aminoácidos.

--- biopython_3.py
+++ biopython_3.py
@@ -35,26 +35,93 @@ print(f"Posiciones de la subsecuencia en la secuencia principal: {resultado}")
 ####################################################################################
 ####################################################################################
 # %%
+#Transcripción de RNA a proteína
 
+ruta_entrada = "C:/Users/Camilo A. Marin Cast/Downloads/Codigos_rosalind/Transcripcion_ARN_PROTEINA/rosalind_prot.txt"
+ruta_salida = "C:/Users/Camilo A. Marin Cast/Downloads/Codigos_rosalind/Transcripcion_ARN_PROTEINA/rosalind_prot_result.txt"
+
+with open (ruta_entrada, "r") as archivo_entrada:
+    secuencia_arn = archivo_entrada.read().strip()
+
+codigo_genetico = {
+    # Fenilalanina y Leucina
+    'UUU': 'F', 'UUC': 'F', 'UUA': 'L', 'UUG': 'L',
+    'CUU': 'L', 'CUC': 'L', 'CUA': 'L', 'CUG': 'L',
+    # Isoleucina y Metionina (Codón de Inicio)
+    'AUU': 'I', 'AUC': 'I', 'AUA': 'I', 'AUG': 'M',
+    # Valina
+    'GUU': 'V', 'GUC': 'V', 'GUA': 'V', 'GUG': 'V',
+    # Serina
+    'UCU': 'S', 'UCC': 'S', 'UCA': 'S', 'UCG': 'S',
+    'AGU': 'S', 'AGC': 'S',
+    # Prolina
+    'CCU': 'P', 'CCC': 'P', 'CCA': 'P', 'CCG': 'P',
+    # Treonina
+    'ACU': 'T', 'ACC': 'T', 'ACA': 'T', 'ACG': 'T',
+    # Alanina
+    'GCU': 'A', 'GCC': 'A', 'GCA': 'A', 'GCG': 'A',
+    # Tirosina y Codones de Parada (Stop)
+    'UAU': 'Y', 'UAC': 'Y', 'UAA': 'Stop', 'UAG': 'Stop',
+    # Histidina y Glutamina
+    'CAU': 'H', 'CAC': 'H', 'CAA': 'Q', 'CAG': 'Q',
+    # Asparagina y Lisina
+    'AAU': 'N', 'AAC': 'N', 'AAA': 'K', 'AAG': 'K',
+    # Ácido Aspártico y Ácido Glutámico
+    'GAU': 'D', 'GAC': 'D', 'GAA': 'E', 'GAG': 'E',
+    # Cisteína, Codón de Parada (Stop) y Triptófano
+    'UGU': 'C', 'UGC': 'C', 'UGA': 'Stop', 'UGG': 'W',
+    # Arginina
+    'CGU': 'R', 'CGC': 'R', 'CGA': 'R', 'CGG': 'R',
+    'AGA': 'R', 'AGG': 'R',
+    # Glicina
+    'GGU': 'G', 'GGC': 'G', 'GGA': 'G', 'GGG': 'G'
+}
+
+proteina = ""
+
+for i in range(0, len(secuencia_arn), 3):
+    codon = secuencia_arn[i:i + 3]
+    aminoacido = codigo_genetico.get(codon)
+
+    if aminoacido == "Stop" or aminoacido is None:
+        break
+
+    proteina += aminoacido
+
+with open (ruta_salida, "w") as archivo_salida:
+    archivo_salida.write(proteina)
 
 # %%
 #RNA splicing
 
-ruta_entrada = "C:/Users/Camilo A. Marin Cast/Downloads/Codigos_rosalind/rosalind_subs.txt"
-ruta_salida = "C:/Users/Camilo A. Marin Cast/Downloads/Codigos_rosalind/rosalind_splc_result.txt"
+ruta_entrada = "C:/Users/Camilo A. Marin Cast/Downloads/Codigos_rosalind/rna_splicing/rosalind_splc.txt"
+ruta_salida = "C:/Users/Camilo A. Marin Cast/Downloads/Codigos_rosalind/rna_splicing/rosalind_splc_result.txt"
+
+secuencias = []
+secuencia_actual = ""
 
 with open (ruta_entrada, "r") as archivo_entrada:
-    secuencia_adn = archivo_entrada.read().strip().splitlines()
+    for linea in archivo_entrada:
+        linea = linea.strip()
 
-# Se guardan SOLO las líneas de secuencia (las que NO comienzan con ">") en una lista
-secuencias = []
-for secuencia_adn in secuencia_adn:
-    if not secuencia_adn.startswith(">"):
-        secuencias.append(secuencia_adn)
+        if linea.startswith(">"):
+            #si se encuenta con un ">", se guarda la secuencia acumulada anterior
+            if secuencia_actual:
+                secuencias.append(secuencia_actual)
+                secuencia_actual = ""
+        else:
+            #si es texto de ADN, se va pegando
+            secuencia_actual += linea
+
+    if secuencia_actual:
+        secuencias.append(secuencia_actual)
 
 #Se definen las variables para la secuencia principal y los intrones
 adn_principal = secuencias[0]  # La primera secuencia es la secuencia principal
 intrones = secuencias[1:]  # Las secuencias restantes son los intrones
+
+#print(f"intrones: {intrones}")
+#print(f"adn_principal: {adn_principal}")
 
 #Se eliminan los intrones de la secuencia principal
 cadena_limpia = adn_principal
@@ -100,7 +167,7 @@ codigo_genetico = {
 }
 
 #Cumple la función de iniciar una variable vacía para almacenar la proteína traducida
-proteina = ""
+proteina: str = ""
 
 #Se traducen los codones a aminoácidos
 for i in range(0, len(secuencia_arn), 3):
@@ -110,10 +177,13 @@ for i in range(0, len(secuencia_arn), 3):
     if aminoacido == "Stop" or aminoacido is None:
         break
 
-proteina += aminoacido
+    proteina += aminoacido
 
-print(f"Secuencia de ARN transcrita: {secuencia_arn}")
-print(f"Total de nucleótidos en la secuencia de ARN: {len(secuencia_arn)}")
-print(f"Total de nucleótidos en la secuencia de ADN: {len(adn_principal)}")
-print(f"Secuencia de proteína traducida: {proteina}")
-print(f"Total de proteinas traducidas: {len(proteina)}")
+with open (ruta_salida, "w") as archivo_salida:
+    archivo_salida.write(proteina)
+
+#print(f"Secuencia de ARN transcrita: {secuencia_arn}")
+#print(f"Total de nucleótidos en la secuencia de ARN: {len(secuencia_arn)}")
+#print(f"Total de nucleótidos en la secuencia de ADN: {len(adn_principal)}")
+#print(f"Secuencia de proteína traducida: {proteina}")
+#print(f"Total de aminoácidos en la proteína traducida: {len(proteina)}")
